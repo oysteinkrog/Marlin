ffc55aa03f76f2ee6eb9a730e4ebb48f9df2511e
Øystein Krog <oystein.krog@gmail.com>
2014-08-10 21:15:33 +0200

    Add new custom motherboard (KROG), for use with RRD fan extender

---
+++ Marlin/pins_KROG.h
@@ -0,0 +1,15 @@
+#define IS_RAMPS_EEB
+
+#define X2_STEP_PIN   64
+#define X2_DIR_PIN    59
+#define X2_ENABLE_PIN 44
+
+#include "pins_RAMPS.h"
+
+#define FAN_PIN             4
+#define SERVO0_PIN         42
+
+#if NUM_SERVOS > 1
+#error Motherboard does not support more than one servo (you can try to define more in pins.h)
+#endif
+
