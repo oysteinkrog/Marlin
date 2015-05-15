bb88609b631bb531bf6e7341f0c81de4239a4412
Øystein Krog <oystein.krog@gmail.com>
2015-05-15 15:54:32 +0200

    adv: extruder fan 50% (40mm)

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -106,12 +106,12 @@
 // extruder temperature is above/below EXTRUDER_AUTO_FAN_TEMPERATURE.
 // Multiple extruders can be assigned to the same pin in which case
 // the fan will turn on when any selected extruder is above the threshold.
-#define EXTRUDER_0_AUTO_FAN_PIN   11
-#define EXTRUDER_1_AUTO_FAN_PIN   11
+#define EXTRUDER_0_AUTO_FAN_PIN   6
+#define EXTRUDER_1_AUTO_FAN_PIN   -1
 #define EXTRUDER_2_AUTO_FAN_PIN   -1
 #define EXTRUDER_3_AUTO_FAN_PIN -1
 #define EXTRUDER_AUTO_FAN_TEMPERATURE 35
-#define EXTRUDER_AUTO_FAN_SPEED   255  // == full speed
+#define EXTRUDER_AUTO_FAN_SPEED   128  // 255 == full speed
 
 
 //===========================================================================
