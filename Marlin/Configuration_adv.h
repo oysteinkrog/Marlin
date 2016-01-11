535471c88a6c294e0f97f4adaf6c6d768075aac5
Øystein Krog <oystein.krog@gmail.com>
2016-01-11 16:29:03 +0100

    config/adv: use full speed for extruder fan

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -111,7 +111,7 @@
 #define EXTRUDER_2_AUTO_FAN_PIN   -1
 #define EXTRUDER_3_AUTO_FAN_PIN -1
 #define EXTRUDER_AUTO_FAN_TEMPERATURE 35
-#define EXTRUDER_AUTO_FAN_SPEED   128  // 255 == full speed
+#define EXTRUDER_AUTO_FAN_SPEED   255  // 255 == full speed
 
 
 //===========================================================================
