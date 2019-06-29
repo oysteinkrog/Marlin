7a05c7e261d0b55ef1ff473b108a12a55438bddd
Øystein Krog <oystein.krog@gmail.com>
2019-06-29 12:48:04 +0200

    fix

--- Marlin/src/pins/pins_KROG.h
+++ Marlin/src/pins/pins_KROG.h
@@ -1,7 +1,7 @@
 #define IS_RAMPS_EEB
 
-//#include "pins_MKS_BASE.h"
-#include "pins_RAMPS.h"
+#include "pins_MKS_GEN_L.h"
+//#include "pins_RAMPS.h"
 
 //#define X2_STEP_PIN   64
 //#define X2_DIR_PIN    59
@@ -29,18 +29,18 @@
 //#define X2_ENABLE_PIN       56
 //#define X2_CS_PIN           49
 //
-#define X_SERIAL_RX 59
+//#define X_SERIAL_RX 59
 
 //#define HEATER_BED_PIN RAMPS_D8_PIN
 //#define HEATER_0_PIN   RAMPS_D9_PIN
 //#define HEATER_1_PIN   RAMPS_D10_PIN
 
-#undef FAN_PIN
-//#define FAN_PIN             RAMPS_D9_PIN
+//#undef FAN_PIN
+//#define FAN_PIN             4
 //#define FAN_PIN            65
 //#define FAN1_PIN            9
 //#define FAN2_PIN            8
-//#define CONTROLLER_FAN_PIN            44
+#define CONTROLLER_FAN_PIN            4
 
 // pt100
 //#define TEMP_0_PIN         4   // Analog Input
